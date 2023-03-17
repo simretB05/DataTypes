897449e780017ec574932ecb3f5d2fc0c3e60b5e
simretB05 <happysimret@gmail.com>
2023-03-16 19:36:07 -0600

    added code to get the last item from an array

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-let username = "simret"
-let user_age = "20"
-let max_volume = 70
-current_volume = 30
-let usernames = ["jon","Snow", "candy", "man"]
- let  users_age=[19,28.58,62,89]
-console.log( " Greetings " + username )
-console.log( `greetings ${ username }` )
-console.log( max_volume / current_volume )
- console.log((max_volume/current_volume)*100)
+let username = "simret";
+let user_age = "20";
+let max_volume = 70;
+current_volume = 30;
+let usernames = ["jon", "Snow", "Candy", "Man"];
+let users_age = [19, 28.58, 62, 89];
+let last_user_name = usernames[3];
+let last_user_names = usernames[-1];
+let middle_user_age =users_age[3]
+console.log( " Greetings " + username );
+console.log( `greetings ${ username }` );
+console.log( max_volume / current_volume );
+console.log( ( max_volume / current_volume ) * 100 );
+console.log( usernames[0] );
+console.log( last_user_names )
+console.log( last_user_name )
+
+
+ 
