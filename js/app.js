eba0d1af4f996b7197e3abe0e4b2c64d80ab76b9
simretB05 <happysimret@gmail.com>
2023-03-16 19:16:16 -0600

    added arrays

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let username = "simret"
+let user_age = "20"
+let max_volume = 70
+current_volume = 30
+let usernames = ["jon","Snow", "candy", "man"]
+ let  users_age=[19,28.58,62,89]
+console.log( " Greetings " + username )
+console.log( `greetings ${ username }` )
+console.log( max_volume / current_volume )
+ console.log((max_volume/current_volume)*100)
